98e28b882c1fac44c6e504a0ac8994de1c12f793
Hadi Ahmed <969hadiahmed@gmail.com>
2023-06-29 15:38:37 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("TTT");
+let music=new Audio("music.mp3");
+let ting=new Audio("ting.mp3");
+let gameOver=new Audio("gameover.mp3");
+let turn="X";
+let isgameover=false;
+//Function to change turn
+const changeTurn=()=>{
+    ting.play();
+    return turn==="X"?"0":"X";
+}
+
+const checkWin=()=>{
+    let boxtext=document.getElementsByClassName("boxtext");
+    let wins=[
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+    wins.forEach(e=>{ 
+        if((boxtext[e[0]].innerText ===boxtext[e[1]].innerText)&&(boxtext[e[0]].innerText ===boxtext[e[2]].innerText)&& boxtext[e[0]].innerText!=''){
+            document.querySelector('.info').innerText=boxtext[e[0]].innerText +" won";
+            console.log("Win");
+            isgameover=true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width='100px';
+
+        }
+    })
+}
+
+//Game Logic
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach((element)=>{
+    let boxtext=element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText==='' && !isgameover){
+            boxtext.innerHTML=turn;
+            turn=changeTurn();
+            checkWin();
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
+        }
+    })
+})
+reset.addEventListener("click",()=>{
+    let boxtext=document.querySelectorAll(".boxtext");
+    Array.from(boxtext).forEach((element)=>{
+        element.innerText='';        
+    })
+    isgameover=false;
+    turn="X";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
